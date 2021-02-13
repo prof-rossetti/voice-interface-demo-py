735cb6c7c7e1ad0442d9df81d918a380ef2f0d48
MJ Rossetti <s2t2@users.noreply.github.com>
2021-02-13 16:16:42 -0500

    RPS game via voice commands

--- app/rps.py
+++ app/rps.py
@@ -1,6 +1,10 @@
 
+from random import choice
 
 from speech_recognition import Recognizer, Microphone
+
+OPTIONS = ["ROCK", "PAPER", "SCISSORS"]
+
 
 class VoiceService:
     def __init__(self):
@@ -14,17 +18,41 @@ class VoiceService:
     def get_transcript(self, audio, show_all=False):
         return self.client.recognize_google(audio, show_all=show_all)
 
+
 if __name__ == '__main__':
 
     service = VoiceService()
 
-    print("----------------")
-    print("HELLO! I AM LISTENING...")
-    print("WHEN YOU ARE READY, PLEASE SAY SOMETHING... ")
-    audio = service.capture_audio()
-    #print(type(audio)) #> <class 'speech_recognition.AudioData'>
+    # USER SELECTION
 
     print("----------------")
-    print("OK! I THINK YOU SAID... ")
+    print("HELLO! I AM LISTENING...")
+    print("WHEN YOU ARE READY, PLEASE SAY 'ROCK', 'PAPER', OR 'SCISSORS'... ")
+    audio = service.capture_audio()
+    #print(type(audio)) #> <class 'speech_recognition.AudioData'>
     transcript = service.get_transcript(audio)
-    print(transcript.upper())
+    transcript = transcript.upper()
+    print(transcript)
+
+    user_choice = None
+    choices = 0
+    for option in OPTIONS:
+        if option in transcript:
+            user_choice = option
+            choices+=1
+    if user_choice == None or choices > 1:
+        print("OOPS PLEASE TRY AGAIN")
+        exit()
+
+    print("----------------")
+    print("YOU CHOSE:")
+    print(user_choice)
+
+    computer_choice = choice(OPTIONS)
+    print("----------------")
+    print("THE COMPUTER CHOSE:")
+    print(computer_choice)
+
+    print("----------------")
+    print("THE WINNER IS...")
+    print("[TODO]")
